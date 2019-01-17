0cb1a0b03fa1819e66c36cd8414645bb98dca485
jaymile <djemil.aminou@gmail.com>
2019-01-17 19:41:24 +0100

    exo deux a finir et le rspec a faire

---
+++ spec/trader_de_lobscur_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/trader_de_lobscur.rb'
+describe "symbole" do
+  it "should return a array hash" do
+    expect(symbole).to be_truthy
+
+  end
+end
+
+describe "price" do
+  it "should return a array hash" do
+    expect(symbole).to be_truthy
+
+  end
+end
