d6f9bd30bcc764f57db231e956def7d1d622100a
jaymile <djemil.aminou@gmail.com>
2019-01-18 10:34:07 +0100

    il reste lexo deputé et le bug de lexo mairie a compredre

--- lib/route_de_la_mairie.rb
+++ lib/route_de_la_mairie.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 PAGE_URL = "http://annuaire-des-mairies.com/val-d-oise.html"
 page = Nokogiri::HTML(open(PAGE_URL))
 
-
 def get_townhall_email(townhall_url)
   doc = Nokogiri::HTML(open(townhall_url))
   doc.xpath('//tbody/tr[4]/td[2]').each do |node|
@@ -23,20 +22,19 @@ doc = Nokogiri::HTML(open(url_ville))
   return ville.map { |e| e.gsub('./' , 'https://www.annuaire-des-mairies.com/')  } # ma methode retourne 'ville' mais vue que jai besoin de supprimer le '.' pour avoir une adreese url correct j'utilise gsub pour enlever le point et la methode map pour la boucle qui me permet de suprimer et de le remplacé par le reste du site
 end
 
-def emailParVille
-i = 0
-variable = []
-  while i < get_townhall_urls.count
-
-      doc = Nokogiri::HTML(open("#{url_ville[i]}"))
-      	doc.xpath('//tr[2]//p//a/@href').map do |node|
-          ville.push(node.text) #ville.push je fait rentré url dans larray et le(node.text) est la valeur de doc.xpath
-
-      	end
-       variable[i]
-       i +=1
-  end
-end
+# def emailParVille
+# i = 0
+# totalUrl = get_townhall_urls.count
+# variable = []
+#   while (i<totalUrl)
+#     doc = Nokogiri::HTML(open(townhall_url))
+#     doc.xpath('//tbody/tr[4]/td[2]').each do |node|
+#       	end
+#        variable[i]
+#        i +=1
+#      end
+#
+# end
 
 
 def perfom
---
+++ spec/route_de_la_mairie_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/route_de_la_mairie.rb'
+describe "get_townhall_email" do
+  it "should return a email" do
+    expect(symbole).to be_truthy
+
+  end
+end
+
+describe "get_townhall_urls" do
+  it "should return a url" do
+    expect(symbole).to be_truthy
+
+  end
+end
